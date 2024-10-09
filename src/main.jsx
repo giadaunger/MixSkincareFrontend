76dbbfab02b3ec002c25bc1b76140270ec443592
Giada Unger <giada.unger02@gmail.com>
2024-10-09 11:23:07 +0200

    Added routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,21 @@
 import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import "./index.css";
 
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+
 function App() {
-  return <div>Hellos</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
