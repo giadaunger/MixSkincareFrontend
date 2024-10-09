1f8323f2b811c302e9d8aa8a5fa0c60a8e1713f3
Giada Unger <giada.unger02@gmail.com>
2024-10-09 11:15:46 +0200

    Created a layout page

---
+++ src/pages/Layout.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+function Layout() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <div className="flex-auto">
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default Layout;
