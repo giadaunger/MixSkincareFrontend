a38f126aefcbec749d66bd99bf55d3259d98a3b2
Giada Unger <giada.unger02@gmail.com>
2024-10-09 14:38:31 +0200

    Deleted the footer

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
 
 function Layout() {
   return (
@@ -10,7 +9,6 @@ function Layout() {
       <div className="flex-auto">
         <Outlet />
       </div>
-      <Footer />
     </div>
   );
 }
