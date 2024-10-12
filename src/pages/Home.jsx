dd27a14358673068a6341a122be6bf129e9946d8
Giada Unger <giada.unger02@gmail.com>
2024-10-12 11:33:57 +0200

    Worked on the frontpage btn

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,17 +2,19 @@ import React from "react";
 
 function Home() {
   return (
-    <div className="relative flex justify-end w-2/3 mx-auto">
-      <div className="absolute left-0 z-20 ml-12">
-        <h1 className="text-6xl">
+    <div className="relative flex justify-end w-5/6 mx-auto">
+      <div className="absolute left-0 z-20 ml-12 mt-36 w-2/3">
+        <h1 className="text-6xl mb-10">
           Instantly Check Skincare Product Compatibility
         </h1>
-        <p className="text-2xl">Safe, Simple, and Smart!</p>
-        <button className="mt-4 bg-blue-500 text-white font-bold py-2 px-4 rounded">
-          Start Matching!
-        </button>
+        <p className="text-4xl mb-20">Safe, Simple, and Smart!</p>
+        <div className="w-1/2 justify-center flex">
+          <button className="mt-4 bg-[#ffb6c1] border border-[#ffb6c1] p-4 rounded-full justify-center w-1/2 text-xl shadow-xl transition duration-300 hover:scale-125 hover:bg-white hover:text-[#ffb6c1]">
+            Start Matching!
+          </button>
+        </div>
       </div>
-      <div className="bg-[#ffb6c1] md:w-2/5 w-1/2 pt-28 rounded-xl mt-16">
+      <div className="bg-[#ffb6c1] md:w-1/2 w-1/2 pt-28 rounded-xl mt-16 shadow-lg">
         <img
           src="/frontPagePic.png"
           alt="Hand holding a jar of cream"
