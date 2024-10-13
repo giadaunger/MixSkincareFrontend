f2c1ef646c5a5f4b195fb74b22f7311630e92c47
Giada Unger <giada.unger02@gmail.com>
2024-10-13 09:33:34 +0200

    Worked on responsive styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,22 +3,24 @@ import React from "react";
 function Home() {
   return (
     <div className="relative flex justify-end w-5/6 mx-auto">
-      <div className="absolute left-0 z-20 ml-12 mt-36 w-2/3">
-        <h1 className="text-6xl mb-10">
+      <div className="absolute left-0 z-20 ml-12 mt-36 lg:w-2/3">
+        <h1 className="text-4xl sm:text-5xl md:text-6xl mb-10">
           Instantly Check Skincare Product Compatibility
         </h1>
-        <p className="text-4xl mb-20">Safe, Simple, and Smart!</p>
-        <div className="w-2/3 justify-center flex">
-          <button className="mt-4 bg-[#ffb6c1] border border-[#ffb6c1] py-4 rounded-full justify-center w-1/2 text-2xl shadow-xl transition duration-300 hover:scale-125 hover:bg-white hover:text-[#ffb6c1]">
+        <p className="text-2xl sm:text-3xl md:text-4xl mb-20 ">
+          Safe, Simple, and Smart!
+        </p>
+        <div className="w-full sm:w-2/3 lg:justify-center flex">
+          <button className="mt-4 bg-[#ffb6c1] border border-[#ffb6c1] py-4 rounded-full w-1/2 text-xl sm:text-2xl shadow-xl transition duration-300 hover:scale-125 hover:bg-white hover:text-[#ffb6c1]">
             Start Matching!
           </button>
         </div>
       </div>
-      <div className="bg-[#ffb6c1] md:w-1/2 w-1/2 pt-28 rounded-xl mt-16 shadow-lg">
+      <div className="bg-[#ffb6c1] w-full lg:w-1/2 pt-28 rounded-xl mt-16 shadow-lg flex mb-0">
         <img
           src="/frontPagePic.png"
           alt="Hand holding a jar of cream"
-          className="mx-auto"
+          className="lg:mx-auto mt-auto ml-auto h-1/2 sm:h-2/3 lg:h-auto"
         />
       </div>
     </div>
