3ef994a7fdf648db3e291cdef0e460940496a314
Giada Unger <giada.unger02@gmail.com>
2024-12-28 14:39:53 +0100

    More styling on the product cards

--- src/pages/CompareProducts.jsx
+++ src/pages/CompareProducts.jsx
@@ -11,16 +11,16 @@ function CompareProducts() {
         </div>
         <div className="flex flex-col md:flex-row justify-center items-center gap-8 md:gap-24">
           <div className="bg-[#ffb6c1] rounded-xl w-60 min-w-60 min-h-96 flex items-center justify-center shadow-xl">
-            <div className="bg-white rounded-full mx-auto items-center w-20 h-20 flex shadow-xl">
-              <Plus className="w-12 h-12 flex mx-auto items-center" color="#ffb6c1"/>
+            <div className="bg-white rounded-full mx-auto items-center w-20 h-20 flex shadow-xl transition duration-300 hover:scale-125">
+              <Plus className="w-12 h-12 flex mx-auto items-center text-[#ffb6c1]"/>
             </div>
           </div>
           <div className="flex justify-center items-center">
-            <Plus className="w-24 h-24" color="#ffb6c1"/>  
+            <Plus className="w-24 h-24 text-[#ffb6c1]"/>  
           </div>
           <div className="bg-[#ffb6c1] rounded-xl w-60 min-w-60 min-h-96 flex items-center justify-center shadow-xl">
-            <div className="bg-white rounded-full mx-auto items-center w-20 h-20 flex shadow-xl">
-              <Plus className="w-12 h-12 flex mx-auto items-center" color="#ffb6c1"/>
+            <div className="bg-white rounded-full mx-auto items-center w-20 h-20 flex shadow-xl transition duration-300 hover:scale-125">
+              <Plus className="w-12 h-12 flex mx-auto items-center text-[#ffb6c1]" />
             </div>
           </div>
         </div>
