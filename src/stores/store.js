f777117d72686f2298d8c32952eb5c0a5588202d
Giada Unger <giada.unger02@gmail.com>
2025-01-06 11:06:48 +0100

    Added a fetch for fetching products

--- src/stores/store.js
+++ src/stores/store.js
@@ -3,6 +3,23 @@ import { create } from "zustand";
 const useStore = create((set) => ({
   isPopupOpen: false,
   setIsPopupOpen: (value) => set({ isPopupOpen: value }),
+
+  chosenProduct: "",
+  fetchProduct: async (chosenProduct) => {
+    try {
+      const respons = await fetch(`http://localhost:8000/product/ingredients/${chosenProduct}`, {
+        method: "GET",
+      });
+      if(!respons.ok) {
+        console.log("Failed to fetch product");
+        throw new Error("Failed to fetch product");
+      }
+      const chosenUserProduct = await respons.json();
+      set({ chosenProduct: chosenUserProduct })
+    } catch (error) {
+      console.log("Error fetching product: ", error)
+    }
+  }
 }));
 
 export default useStore;
