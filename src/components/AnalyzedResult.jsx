9484a2a0220891bc3efb24e230740f153e08f84b
Giada Unger <giada.unger02@gmail.com>
2025-01-07 15:27:46 +0100

    Added msg if no warnings

--- src/components/AnalyzedResult.jsx
+++ src/components/AnalyzedResult.jsx
@@ -1,60 +1,73 @@
 import React from "react";
 import useStore from "../stores/store";
 import { Warning } from "@styled-icons/ionicons-solid/Warning";
+import { Ok } from "styled-icons/crypto";
 
 function AnalyzedResult() {
   const { analysisResult } = useStore();
+
+  const hasActiveIngredients = analysisResult.active_ingredients?.length > 0;
+  const hasIncompatibilities = analysisResult.incompatibility_warnings?.length > 0;
   
   return (
-    <div className="bg-[#ffb6c1] rounded-xl p-6 lg:w-2/3 mx-auto mb-24">
-      <div className="md:flex inline">
-        <div className="bg-white rounded-xl p-6 md:w-3/4">
-          {analysisResult.active_ingredients?.map((active, index) => (
-            <div 
-              key={index} 
-              className="sm:flex w-full items-center gap-3 bg-[#fffcc8] text-[#e3ba5f] font-bold rounded-xl sm:rounded-full mt-4 px-6 py-3 max-w-2xl mx-auto"
-            >
-              <Warning className="w-8 h-8 flex-shrink-0" />
-              <span className="text-center">
-                Active ingredient: {active.ingredient}
-                {active.requires_sunscreen && ". Wear sunscreen, when used in AM"}
-                {active.additional_info && `. ${active.additional_info}`}
-              </span>
+    <>
+      {(hasActiveIngredients || hasIncompatibilities) ? (
+        <div className="bg-[#ffb6c1] rounded-xl p-6 lg:w-2/3 mx-auto mb-24">
+          <div className="md:flex inline">
+            <div className="bg-white rounded-xl p-6 md:w-3/4">
+              {analysisResult.active_ingredients?.map((active, index) => (
+                <div 
+                  key={index} 
+                  className="sm:flex w-full items-center gap-3 bg-[#fffcc8] text-[#e3ba5f] font-bold rounded-xl sm:rounded-full mt-4 px-6 py-3 max-w-2xl mx-auto shadow-xl"
+                >
+                  <Warning className="w-8 h-8 flex-shrink-0" />
+                  <span className="text-center">
+                    Active ingredient: {active.ingredient}
+                    {active.requires_sunscreen && ". Wear sunscreen, when used in AM"}
+                    {active.additional_info && `. ${active.additional_info}`}
+                  </span>
+                </div>
+              ))}
+              {analysisResult.incompatibility_warnings?.map((warning, index) => (
+                <div 
+                  key={index}
+                  className="sm:flex w-full items-center gap-3 bg-[#cef4fa] text-[#7ac4d1] font-bold rounded-xl sm:rounded-full mt-4 px-6 py-3 max-w-2xl mx-auto shadow-xl"
+                >
+                  <Warning className="w-8 h-8 flex-shrink-0" />
+                  <span className="text-center">
+                    Ingredient {warning.ingredients[0]} and {warning.ingredients[1]} are not compatible!
+                    {warning.message && ` (${warning.message})`}
+                  </span>
+                </div>
+              ))}
             </div>
-          ))}
-
-          {analysisResult.incompatibility_warnings?.map((warning, index) => (
-            <div 
-              key={index}
-              className="sm:flex w-full items-center gap-3 bg-[#cef4fa] text-[#7ac4d1] font-bold rounded-xl sm:rounded-full mt-4 px-6 py-3 max-w-2xl mx-auto"
-            >
-              <Warning className="w-8 h-8 flex-shrink-0" />
-              <span className="text-center">
-                Ingredient {warning.ingredients[0]} and {warning.ingredients[1]} are not compatible!
-                {warning.message && ` (${warning.message})`}
-              </span>
+            <div className="flex flex-1 items-center">
+              {analysisResult.active_ingredients && !analysisResult.incompatibility_warnings &&
+                <div className="w-40 h-40">
+                  <img src="../../yellowWarning.png" alt="" />
+                </div>
+              }
+              {!analysisResult.active_ingredients && analysisResult.incompatibility_warnings &&
+                <div className="w-40 h-40">
+                  <img src="../../blueWarning.png" alt="" />
+                </div>
+              }
+              {analysisResult.active_ingredients && analysisResult.incompatibility_warnings &&
+                <div className="w-40 h-40 mx-auto">
+                  <img src="../../comboWarning.png" alt="" />
+                </div>
+              }
             </div>
-          ))}
+          </div>
         </div>
-        <div className="flex flex-1 items-center">
-          {analysisResult.active_ingredients && !analysisResult.incompatibility_warnings &&
-            <div className="w-40 h-40">
-              <img src="../../yellowWarning.png" alt="" />
-            </div>
-          }
-          {!analysisResult.active_ingredients && analysisResult.incompatibility_warnings &&
-            <div className="w-40 h-40">
-              <img src="../../blueWarning.png" alt="" />
-            </div>
-          }
-          {analysisResult.active_ingredients && analysisResult.incompatibility_warnings &&
-            <div className="w-40 h-40 mx-auto">
-              <img src="../../comboWarning.png" alt="" />
-            </div>
-          }
+      ) : (
+        <div className="sm:flex w-full items-center gap-3 bg-[#d9f9d9] text-[#7dcc7d] font-bold rounded-xl sm:rounded-full mb-20 px-6 py-3 max-w-2xl mx-auto shadow-xl">
+          <Ok className="w-8 h-8 flex-shrink-0" />
+          <span className="text-center">No active ingredients and no incompatible ingredients!</span>
         </div>
-      </div>
-    </div>
+      )
+    }
+  </>
   );
 }
 
