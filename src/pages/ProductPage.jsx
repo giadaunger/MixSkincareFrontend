02537c0e4c303bd28eb0c64b8515924b2fa35499
Giada Unger <giada.unger02@gmail.com>
2025-01-11 10:35:13 +0100

    Responsive design

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -18,10 +18,10 @@ function ProductPage() {
   return (
     <div className="">
       <GoBackBtn />
-      <div className="w-full md:w-2/3 mx-auto flex flex-row justify-center gap-20">
-        <img src={productInfo.product_img} alt="" />
+      <div className="w-5/6 md:w-2/3 mx-auto flex flex-col min-[1000px]:flex-row justify-center gap-20">
+        <img src={productInfo.product_img} alt="" className="h-96 w-72 lg:h-auto lg:w-auto mx-auto" />
         <div className="">
-          <div className="mb-32">
+          <div className="mb-32 text-center min-[1000px]:text-left">
             <h2 className="text-4xl">{productInfo.product_name}</h2>
             <p className="text-xl">{productInfo.company_name}</p>
             <p>{productInfo.category}</p>
@@ -61,27 +61,29 @@ function ProductPage() {
                 SKIN TYPE
               </button>
             </div>
-            {activeTab === 'description' && (
-              <div>
-                <p>{productInfo.description}</p>
-              </div>
-            )}
-            {activeTab === 'ingredients' && (
-              <div>
-                {productInfo.ingredients?.map((ingredientObj) => (
-                  <p key={ingredientObj.id} className="inline mr-4">{ingredientObj.ingredient.ingredient},</p>
-                ))}
-              </div>
-            )}
-            {activeTab === 'SkinType' && (
-              <div>
-                <p>This product is compatible with skintype: a, b and c</p>
-              </div>
-            )}
+            <div className="w-11/12 mx-auto">
+              {activeTab === 'description' && (
+                <div>
+                  <p>{productInfo.description}</p>
+                </div>
+              )}
+              {activeTab === 'ingredients' && (
+                <div>
+                  {productInfo.ingredients?.map((ingredientObj) => (
+                    <p key={ingredientObj.id} className="inline mr-4">{ingredientObj.ingredient.ingredient},</p>
+                  ))}
+                </div>
+              )}
+              {activeTab === 'SkinType' && (
+                <div>
+                  <p>This product is compatible with skintype: a, b and c</p>
+                </div>
+              )}
+            </div>
           </div>
         </div>
       </div>
-      <div className="w-1/3 mx-auto mt-40 space-y-10">
+      <div className="w-5/6 md:w-2/3 mx-auto mt-40 space-y-10 mb-20">
         <div className="bg-[#ffb6c1] w-full rounded-md flex flex-row p-4 justify-between items-center">
           <div>Logo</div>
           <div className="flex flex-row space-x-5 items-center">
