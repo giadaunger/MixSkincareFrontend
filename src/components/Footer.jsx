ac951b619edb49cc4c11cfc7d853c549d2dccad3
Giada Unger <giada.unger02@gmail.com>
2025-05-01 21:45:29 +0200

    Added footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { NavLink } from "react-router-dom";
+
+function Footer() {
+  return (
+    <footer className="p-4 shadow-md">
+      <div className="container mx-auto flex flex-col min-[1110px]:flex-row items-center justify-between">
+        <div>
+          <NavLink to="/">
+            <img
+              src="/logo.png"
+              alt="Logo"
+              className="min-[1110px]:h-28 min-[1110px]:w-28 h-20 w-20"
+            />
+          </NavLink>
+        </div>
+        <div className="flex flex-col min-[1110px]:items-end">
+          <NavLink
+            to="/about"
+            className="block px-4 py-1 text-xl transform transition duration-300 hover:scale-110 hover:text-[#E2A3B7]"
+          >
+            About
+          </NavLink>
+          <NavLink
+            to="/privacy-policy"
+            className="block px-4 py-1 text-xl transform transition duration-300 hover:scale-110 hover:text-[#E2A3B7]"
+          >
+            Privacy Policy
+          </NavLink>
+          <NavLink
+            to="/cookie-policy"
+            className="block px-4 py-1 text-xl transform transition duration-300 hover:scale-110 hover:text-[#E2A3B7]"
+          >
+            Cookie Policy
+          </NavLink>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 function Layout() {
   return (
@@ -9,6 +10,7 @@ function Layout() {
       <div className="flex-auto">
         <Outlet />
       </div>
+      <Footer />
     </div>
   );
 }
